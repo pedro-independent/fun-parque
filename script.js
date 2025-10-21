e4dc49030000b4e8be9556fab6d2b75dcd2e70fa
Nevvees <pedro@independent.pt>
2025-10-21 11:41:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -749,3 +749,48 @@ function initPricingFilter() {
 initPricingFilter();
 
 
+/* Testimonials Slider */
+function initSwiperSlider() {  
+  const swiperSliderGroups = document.querySelectorAll("[data-swiper-group]");
+  
+  swiperSliderGroups.forEach((swiperGroup) => {
+    const swiperSliderWrap = swiperGroup.querySelector("[data-swiper-wrap]");
+    if(!swiperSliderWrap) return;
+    
+    const prevButton = swiperGroup.querySelector("[data-swiper-prev]");
+    const nextButton = swiperGroup.querySelector("[data-swiper-next]");
+    
+    const swiper = new Swiper(swiperSliderWrap, {
+      slidesPerView: 1.25,
+      speed: 600,
+      mousewheel: true,
+      grabCursor: true,
+      breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1.8,
+        },
+        // when window width is >= 992px
+        992: {
+          slidesPerView: 3.5,
+        }
+      },
+      navigation: {
+        nextEl: nextButton,
+        prevEl: prevButton,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },    
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },      
+    });    
+    
+  });
+}
+
+initSwiperSlider();
